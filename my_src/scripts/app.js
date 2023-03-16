802347e8016c24ff7d5669df8e2434a1fee94d23
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-03-16 14:37:35 +0100

    rename public dir to my_src, reorder app.js funcs

--- my_src/scripts/app.js
+++ my_src/scripts/app.js
@@ -1,7 +1,7 @@
+import 'canvas-confetti';
+
 let timeLeft = 0;
 let desiredTime = 0;
-let elapsedTime = 0;
-let running = false;
 let timerInterval = null;
 const alertThreshold = 0.1;
 const warningThreshold = 0.25;
@@ -22,8 +22,8 @@ const inputDiv = document.querySelector('.input-div');
 const buttonDiv = document.querySelector('.button-div');
 const sound = new Audio('assets/handpan.wav');
 sound.volume = 0.45;
-
 const rootStyles = getComputedStyle(htmlEl);
+const confetti = require('canvas-confetti');
 
 const OLD_COLOR_CODES = {
   info: { color: '#2DAC36' }, // green
@@ -45,6 +45,14 @@ const COLOR_CODES = {
   },
 };
 
+/* Utility function to zero-pad numbers when needed */
+function zeroPad(unit) {
+  if (unit < 10) {
+    return `0${unit}`;
+  }
+  return unit;
+}
+
 /* Returns a string of the time passed as an argument */
 function formatTimeLeft(time) {
   // user-set time in seconds
@@ -61,12 +69,33 @@ function formatTimeLeft(time) {
 
 baseTimerLabel.innerText = formatTimeLeft(timeLeft);
 
-/* Utility function to zero-pad numbers when needed */
-function zeroPad(unit) {
-  if (unit < 10) {
-    return `0${unit}`;
+// Divides time left by the defined time limit.
+function calculateTimeFraction() {
+  return timeLeft / desiredTime;
+}
+
+// Update the dasharray value as time passes, starting with 283
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`; // first number is remaining, second is total (283)
+  document
+    .getElementById('base-timer-path-remaining')
+    .setAttribute('stroke-dasharray', circleDasharray);
+}
+
+function setRemainingPathColor(remainingTime) {
+  const { alert, warning, info } = COLOR_CODES;
+  // If the remaining time is less than or equal to 10%, change the color to red
+  if (remainingTime <= alertTime) {
+    htmlEl.style.setProperty('--remaining-path-color', alert.color);
+    // If the remaining time is less than or equal to 25%, change the color to orange
+  } else if (remainingTime <= warningTime) {
+    htmlEl.style.setProperty('--remaining-path-color', warning.color);
+    // Otherwise, set the color to green
+  } else {
+    htmlEl.style.setProperty('--remaining-path-color', info.color);
   }
-  return unit;
 }
 
 function startTimer() {
@@ -78,7 +107,7 @@ function startTimer() {
   });
   timerInterval = setInterval(() => {
     // Decrement the time left by 1 second
-    timeLeft--;
+    timeLeft -= 1;
 
     // Update time fraction, redraw circle
     baseTimerLabel.innerHTML = formatTimeLeft(timeLeft);
@@ -97,35 +126,6 @@ function startTimer() {
   }, 1000);
 }
 
-// Divides time left by the defined time limit.
-function calculateTimeFraction() {
-  return timeLeft / desiredTime;
-}
-
-// Update the dasharray value as time passes, starting with 283
-function setCircleDasharray() {
-  const circleDasharray = `${(
-    calculateTimeFraction() * FULL_DASH_ARRAY
-  ).toFixed(0)} 283`; // first number is remaining, second is total (283)
-  document
-    .getElementById('base-timer-path-remaining')
-    .setAttribute('stroke-dasharray', circleDasharray);
-}
-
-function setRemainingPathColor(timeLeft) {
-  const { alert, warning, info } = COLOR_CODES;
-  // If the remaining time is less than or equal to 10%, change the color to red
-  if (timeLeft <= alertTime) {
-    htmlEl.style.setProperty('--remaining-path-color', alert.color);
-    // If the remaining time is less than or equal to 25%, change the color to orange
-  } else if (timeLeft <= warningTime) {
-    htmlEl.style.setProperty('--remaining-path-color', warning.color);
-    // Otherwise, set the color to green
-  } else {
-    htmlEl.style.setProperty('--remaining-path-color', info.color);
-  }
-}
-
 /* Start button event listener */
 startBtn.addEventListener('click', () => {
   startBtn.style.display = 'none';
@@ -134,13 +134,13 @@ startBtn.addEventListener('click', () => {
   titleEl.style.display = 'none';
   if (desiredTime > 0) {
     // unpause
-    clearInterval(timerInterval); //clear flashing time
+    clearInterval(timerInterval); // clear flashing time
     startTimer();
   } else {
     // not in paused state
-    let hours = hoursEl.value;
-    let minutes = minutesEl.value;
-    let seconds = secondsEl.value;
+    const hours = hoursEl.value;
+    const minutes = minutesEl.value;
+    const seconds = secondsEl.value;
     desiredTime = hours * 3600 + minutes * 60 + seconds * 1;
     alertTime = desiredTime * alertThreshold;
     warningTime = desiredTime * warningThreshold;
@@ -187,10 +187,7 @@ document.getElementById('reset-btn').addEventListener('click', () => {
   secondsEl.value = '';
   startBtn.style.display = 'block';
   baseTimerLabel.innerHTML = formatTimeLeft(desiredTime);
-  htmlEl.style.setProperty(
-    '--remaining-path-color',
-    COLOR_CODES.info.color
-  );
+  htmlEl.style.setProperty('--remaining-path-color', COLOR_CODES.info.color);
   setCircleDasharray();
 });
 

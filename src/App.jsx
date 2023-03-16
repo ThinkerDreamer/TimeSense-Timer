b50b55672796eea2e5fb7b4012d1ded73cd8595c
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-03-16 14:21:13 +0100

    add vite boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import logo from './assets/images/logo.svg';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <p className="header">Vite React Starter 💯</p>
+        <p>
+          Vite + React <br />
+          ESLint + Prettier + Stylelint
+          <br />
+          Sass + Emotion + Tailwind
+          <br />
+          Jest + Testing Library
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

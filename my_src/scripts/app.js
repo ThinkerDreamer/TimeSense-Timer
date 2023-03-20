abd76a2b8f4126746819ac1579f5f135f49dc66b
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-03-20 11:36:15 +0100

    save old React jsx for timer component as comment

--- my_src/scripts/app.js
+++ my_src/scripts/app.js
@@ -198,3 +198,36 @@ popOutIcon.addEventListener('click', () => {
     'popup, toolbar=no, menubar=no, status=no, height=555, width=350'
   );
 });
+
+      // {
+      //   /* 
+      // 'min(70vw, 400px)'
+      // <div className={styles.baseTimer}>
+      //   <svg
+      //     className={styles.svg}
+      //     viewBox="0 0 100 100"
+      //     xmlns="http://www.w3.org/2000/svg"
+      //   >
+      //     <g className={styles.circle}>
+      //       <circle
+      //         className={styles.pathElapsed}
+      //         cx="50"
+      //         cy="50"
+      //         r="45"
+      //       />
+      //       <path
+      //         id="base-timer-path-remaining"
+      //         strokeDasharray={circleDasharray}
+      //         className={`${styles.pathRemaining} ${styles.remainingPathColor}`}
+      //         d="
+      //                         M 50, 50
+      //                         m -45, 0
+      //                         a 45,45 0 1,0 90,0
+      //                         a 45,45 0 1,0 -90,0
+      //                       "
+      //       />
+      //     </g>
+      //   </svg>
+      //   <span id="base-timer-label" className={styles.label}></span>
+      // </div> */
+      // }

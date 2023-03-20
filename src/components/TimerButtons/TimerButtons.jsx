2f4bf2b474a4f921bedbea5f148f2a3740e6f204
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-03-20 11:35:16 +0100

    add handlers for all buttons

--- src/components/TimerButtons/TimerButtons.jsx
+++ src/components/TimerButtons/TimerButtons.jsx
@@ -1,24 +1,43 @@
 import React from 'react';
 import styles from './TimerButtons.module.css';
 
-function TimerButtons({ showPause, handleStart }) {
+function TimerButtons({
+  showPause,
+  showStart,
+  showReset,
+  handleStart,
+  handleReset,
+  handlePause,
+}) {
   return (
-    <div class={styles.buttonDiv}>
-      <button
-        id="start-btn"
-        className={styles.button}
-        onClick={handleStart}
-      >
-        Start
-      </button>
+    <div className={styles.buttonDiv}>
+      {showStart && (
+        <button
+          id="start-btn"
+          className={styles.button}
+          onClick={handleStart}
+        >
+          Start
+        </button>
+      )}
       {showPause && (
-        <button id="pause-btn" className={styles.button}>
+        <button
+          id="pause-btn"
+          className={styles.button}
+          onClick={handlePause}
+        >
           Pause
         </button>
       )}
-      <button id="reset-btn" className={styles.button}>
-        Reset
-      </button>
+      {showReset && (
+        <button
+          id="reset-btn"
+          className={styles.button}
+          onClick={handleReset}
+        >
+          Reset
+        </button>
+      )}
     </div>
   );
 }
